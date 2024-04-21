7d00161455fe9f0ad56377684ecead848ecd7aa5
Alberto <alberto.cst05@gmail.com>
2024-04-21 21:41:44 +0200

    acabado

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -1,23 +1,242 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MusicManager : MonoBehaviour
 {
-    [SerializeField] private AudioClip[] songsArray;
-    [SerializeField] private AudioSource cameraAudiosource;
 
-    private float timeBetweenSongs;
+    // reference of UiManager Script
+
+    private UiManager uiManagerScript;
+
+    // songs array and array index variable
+
+    [SerializeField] private AudioClip[] songsArray;
+    private int songNumber = 0;
+
+    //audiosource reference
+
+    public AudioSource cameraAudiosource;
+    
+    // condiions for detect if rndom and loop modes are activated or not
+
+    private bool songIsOnLooop = false;
+    private bool randomModeActivated = false;
+ 
+    // reference of the random toggle to dissable it when loop is activated
+
+    [SerializeField] private Toggle randomToggle;
+
+    
 
     private void Start()
     {
-        AsignSong();
+        //reference of UiManager
+        uiManagerScript = FindObjectOfType<UiManager>();
+
+
+        StartMusic();
      
     }
-    private void AsignSong()
+    private void Update()
     {
-        cameraAudiosource.clip = songsArray[0];
+        NextSongAtFinishCurrentSong();
+        RandomReproduction();
+    }
+
+    // normal mode funcions
+
+    private void StartMusic()
+    {
+        cameraAudiosource.clip = songsArray[songNumber];
         cameraAudiosource.Play();
+        uiManagerScript.ChangeSongName(songNumber);
+        uiManagerScript.ChangeImageSong(songNumber);
+        
+    }
+    private void NextSongAtFinishCurrentSong()
+    {
+        if (cameraAudiosource.time >= cameraAudiosource.clip.length && !songIsOnLooop && !randomModeActivated)
+        {
+            songNumber++;
+            cameraAudiosource.clip = songsArray[songNumber];
+            cameraAudiosource.Play();
+            uiManagerScript.ChangeSongName(songNumber);
+            uiManagerScript.ChangeImageSong(songNumber);
+
+
+        }
+    }
+    public void PauseAndPlay()
+    {
+        if (cameraAudiosource.isPlaying)
+        {
+            cameraAudiosource.Pause();
+        }
+        else
+        {
+            cameraAudiosource.UnPause();
+        }
+    }
+    public void NextSongButton()
+    {
+        if (!randomModeActivated && !songIsOnLooop)
+        {
+            songNumber++;
+
+            if (songNumber > songsArray.Length - 1 )
+            {
+                songNumber = 0;
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+            }
+            else if (songNumber <= songsArray.Length - 1 )
+            {
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+            }
+        }
+        else if (randomModeActivated && !songIsOnLooop)
+        {
+            
+            
+            if ( songNumber < songsArray.Length - 1)
+            {
+                songNumber = Random.Range(songNumber, songsArray.Length - 1);
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+            }
+            else if (songNumber >= 14) 
+            {
+                songNumber = Random.Range(0, songsArray.Length - 1);
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+
+            }
+
+
+
+
+        }
+        
+       
+    }
+    public void PreviusSongButton()
+    {
+        if (!randomModeActivated && !songIsOnLooop)
+        {
+            songNumber--;
+
+            if (songNumber < 0 && !randomModeActivated)
+            {
+                songNumber = songsArray.Length - 1;
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+            }
+            else if (songNumber >= 0 && !songIsOnLooop && !randomModeActivated)
+            {
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+            }
+        }
+        else if (randomModeActivated && !songIsOnLooop)
+        {
+
+
+            if (songNumber > 0)
+            {
+                songNumber = Random.Range(0, songNumber);
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+            }
+            else if (songNumber <= 0)
+            {
+                songNumber = Random.Range(0, songsArray.Length - 1);
+                cameraAudiosource.clip = songsArray[songNumber];
+                cameraAudiosource.Play();
+                uiManagerScript.ChangeSongName(songNumber);
+                uiManagerScript.ChangeImageSong(songNumber);
+
+
+            }
+
+
+
+
+        }
+
+    }
+
+    // loop mode functions
+    
+    public void Loop()
+    {
+        if (!songIsOnLooop)
+        {
+            songIsOnLooop = true;
+            cameraAudiosource.loop = true;
+            randomToggle.interactable = false;
+            
+        }
+        else
+        {
+            songIsOnLooop = false;
+            cameraAudiosource.loop = false;
+            randomToggle.interactable = true;
+        }
+    }   
+    
+    //random mode functions
+
+    public void RandomToggle()
+    {
+        if (!randomModeActivated)
+        {
+            randomModeActivated = true;
+
+        }
+        else
+        {
+            randomModeActivated = false;
+        }
+
+    }
+    private void RandomReproduction()
+    {
+        if (cameraAudiosource.time >= cameraAudiosource.clip.length && !songIsOnLooop && randomModeActivated)
+        {
+            songNumber = Random.Range(0, songsArray.Length - 1);
+            cameraAudiosource.clip = songsArray[songNumber];
+            cameraAudiosource.Play();
+            uiManagerScript.ChangeSongName(songNumber);
+            uiManagerScript.ChangeImageSong(songNumber);
+
+
+
+        }
+
     }
     
+    
+
 }
--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // scripts referencees 
+   
+    private MusicManager musicManagerScript;
+  
+    // ui elements
+
+    [SerializeField] private TextMeshProUGUI playButtonText;
+    [SerializeField] private TextMeshProUGUI songNameText;
+    [SerializeField] private Image songImage;
+
+    // song name array and song image array
+
+    [SerializeField] private string[] songNames;
+    [SerializeField] private Sprite[] songImagesArray;
+
+    private void Start()
     {
-        
+        playButtonText.text = "PLAY";
+
+        //music manager reference
+
+        musicManagerScript = FindObjectOfType<MusicManager>();
+    }
+    
+    //Play buton change text function (when the song is playing it showa "play" and when it is paused it shows "pause")
+
+    public void ChaangePlayButtonText()
+    {
+       if (musicManagerScript.cameraAudiosource.isPlaying)
+        {
+            playButtonText.text = "PLAY";
+        }
+        else
+        {
+            playButtonText.text = "PAUSE";
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    // Change Song Name function
+
+    public void ChangeSongName(int songNameArrayIndex)
     {
-        
+        songNameText.text = songNames[songNameArrayIndex];
     }
+    public void ChangeImageSong(int songImageArrayIndex)
+    {
+        songImage.sprite = songImagesArray[songImageArrayIndex];
+    }
+
+
 }
